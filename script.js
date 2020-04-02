af071ef3c8ecdf9b5b03bfb8a388c11164bfc908
kross <662242@mail.ru>
2020-04-02 16:09:37 +0500

    feat:01 lesson06

--- script.js
+++ script.js
@@ -8,31 +8,32 @@ const comp=(q)=>{
 if(q === true || q > 0){
     let b = Math.ceil(Math.random()*100);
     console.log('искомое число',b);
+let h='введите чилсо';
 
-    const zapros=()=>{    //функция выполняюшая запрс и проверку на число
-        let a = +prompt('введи число','55');  // сам запрос 
+    const zapros=(h)=>{    //функция выполняюшая запрс и проверку на число
+        let a = +prompt(h);  // сам запрос 
       if(!IsNamber(a)){
           console.log( 'повтор запроса ');
           alert('введите число!');
-          return zapros();
+          return zapros(h);
       }else{
         return a;
       }
     };
     
 const comparison=(a,b)=>{
-   
+   let ot='';
      if(a < b && a !== 0){
-         alert('загаданное число больше');
             console.log('загаданное число больше');
-            comparison(zapros(),b);
+           ot = 'загаданное число больше, введите число';
+            comparison(zapros(ot),b);
     }else if(a === b && a !== 0){
             console.log('число равно угадал играем еще');
             comp(confirm('угадал !! с играем еще?'));
     }else if(a > b && a !== 0){
-        alert('загаданное число меньше');
-            console.log('загаданное число меньше');
-   comparison(zapros(),b);
+        ot = 'загаданное число меньше';
+            console.log('загаданное число меньше, введите число');
+   comparison(zapros(ot),b);
     }else if(a === 0){
         console.log('отмена игры');
     }
