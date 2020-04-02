a14975af7ff72b64992c04afc40503a4f42749d3
kross <662242@mail.ru>
2020-04-02 23:02:04 +0500

    feat:fix 05 lesson06

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
 'use strict';
 
-let IsNamber=(n)=>{
-    return !isNaN  (parseFloat(n)) && isFinite(n);
+let IsNamber =(n)=>{
+    return !isNaN  (parseFloat(n));
    };
 
-const comp=(q)=>{
-if(q === true || q > 0){
+const comp = (q) =>{
+//...............................variable
+    let ot='';
     let b = Math.ceil(Math.random()*100);
     console.log('искомое число',b);
-let h='введите чилсо';
-
+    console.log(q);
+//...............................function
     const quest=(h)=>{    //функция выполняюшая запрс и проверку на число
-        let a = prompt(h);  // сам запрос
-      if(a !== null){
-      if(!IsNamber(a)||a === ''){
+        let a = prompt(h);
+    if(a!==null){                                    // сам запрос
+        if(!IsNamber(a) || a === ''){                        // проверка на число и пустую строку
           console.log( 'повтор запроса ');
           alert('повтор введите число!');
           return quest(h);
-      } else{
-        return a;
-      }
+        }else{
+                return a;
+        }
     }else{
-        alert('отмена');
         return;
     }
-
-
     };
-    let ot='';
-const comparison=(a,b)=>{
-  
-     if(a < b && a ){
-            console.log('загаданное число больше');
-           ot = 'загаданное число больше, введите число';
-            comparison(quest(ot),b);
-    }else if(a === b && a ){
-            console.log('число равно угадал играем еще');
-            comp(confirm('угадал !! с играем еще?'));
-    }else if(a > b && a ){
-        ot = 'загаданное число меньше';
-            console.log('загаданное число меньше, введите число');
-   comparison(quest(ot),b);
-    }else if(a === 101 ){
-        console.log('отмена игры');
-    }
-  };
+    const comparison=(a,b)=>{
+        if(a < b  ){
+               alert('загаданное число больше');
+              ot = 'число больше, введите число';
+                         comparison(quest(ot),b);
+       }else if(a > b ){
+           ot = 'загаданное число меньше';
+           alert('число меньше, введите число');
+                        comparison(quest(ot),b);
+       }else if(a === b){
+           console.log('угадал !!');
+            return comp(confirm('угадал !! с играем еще?'));//не запускается
+       }else if(a === null ){
+           alert('игра закончена');
+           return;
+       }
+     };
+//.......................................end function
+
+ if( q === true || q === 1 ){
+
   ot = 'начало игры введите число';
   comparison(quest(ot),b);
 }else{
-    alert('игра закончена');
-}
+     return; 
+  }
 };
 
 
