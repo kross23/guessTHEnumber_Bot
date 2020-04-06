98eff5d369794cd7e2aa4113811e087f34704e96
kross <662242@mail.ru>
2020-04-06 20:44:04 +0500

    fix 04

--- script01.js
+++ script01.js
@@ -7,35 +7,35 @@ let IsNamber = (n) => {
     return !isNaN(parseFloat(n)) && n !== '';
 };
 
-const quest =(h) => { //функция выполняюшая запрс и проверку на число
+const quest = (h) => { //функция выполняюшая запрс и проверку на число
     let a = prompt(h);
     if (a === null) {
-        console.log('a == null==', a );
+        console.log('a == null==', a);
         return a;
-     } else{// сам запрос
-     if (!IsNamber(a)) { // проверка на число и пустую строку
-         console.log('повтор запроса ');
-         alert('повтор введите число!');
-         return quest(h);
-     } else {
-         a = +a;
-         return a;
-     }
- }
+    } else { // сам запрос
+        if (!IsNamber(a)) { // проверка на число и пустую строку
+            console.log('повтор запроса ');
+            alert('повтор введите число!');
+            return quest(h);
+        } else {
+            a = +a;
+            return a;
+        }
+    }
 };
 
-const check =  (n)=> {
-    
+const check = (n) => {
+
     return function (r) {
         if (n > r && r !== null) {
             return 'число больше';
-        } else if (n < r ) {
+        } else if (n < r) {
             return 'число меньше';
         } else if (n === r) {
             return 'число равно';
         } else if (r === null) {
             return 'вы нажали отмена';
-            
+
         }
     };
 };
@@ -43,46 +43,44 @@ const check =  (n)=> {
 let checker = check(b);
 
 const apend = (qw) => {
-    console.log('b' , b);
-        let ert = checker(quest(' ВВЕДИТЕ ЧИСЛО'));
-        if(qw>0){
+    console.log('b', b);
+    let ert = checker(quest(' ВВЕДИТЕ ЧИСЛО'));
+    if (qw > 0) {
         if (ert === 'число больше') {
-                alert('число больше: попыток осталось '+qw);
-                qw--;
-                apend(qw);
-            } else if (ert === 'число меньше') {
-                alert('число меньше : попыток осталось '+qw);
-                qw--;
-                apend(qw);
-            } else if (ert === 'число равно') {
-                alert('число равно угадал');
-                if (confirm('повторить')){
-                    b=Math.ceil(Math.random() * 100);
-                    checker = check(b);
-                    apend(9);
-                }else{
-                    alert('вы нажали отмена');
-                    return 0;
-                }
-                apend(qw);
-            } else if (ert === 'вы нажали отмена') {
-                alert('вы нажали отмена');  
-                return;
-            }
-        }else{
-            alert('попыток не осталось');
-            if (confirm('повторить')){
-                b=Math.ceil(Math.random() * 100);
+            alert('число больше: попыток осталось ' + qw);
+            qw--;
+            apend(qw);
+        } else if (ert === 'число меньше') {
+            alert('число меньше : попыток осталось ' + qw);
+            qw--;
+            apend(qw);
+        } else if (ert === 'число равно') {
+            alert('число равно угадал');
+            if (confirm('повторить')) {
+                b = Math.ceil(Math.random() * 100);
                 checker = check(b);
                 apend(9);
-            }else{
+            } else {
                 alert('вы нажали отмена');
                 return 0;
             }
-            return 0;
+            apend(qw);
+        } else if (ert === 'вы нажали отмена') {
+            alert('вы нажали отмена');
+            return;
         }
-            
+    } else {
+        alert('попыток не осталось');
+        if (confirm('повторить')) {
+            b = Math.ceil(Math.random() * 100);
+            checker = check(b);
+            apend(9);
+        } else {
+            alert('вы нажали отмена');
+            return ;
+        }
 
-    };
+    }
+};
 
-    apend(9);
+apend(9);
